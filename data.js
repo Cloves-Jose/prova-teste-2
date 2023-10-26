5dc2dba42d4943fc837c9ff5129e07699f7adaee
Cloves Jose <clovesjsilva8@gmail.com>
2023-10-25 23:27:46 -0300

    Prova de recuperação

--- data.js
+++ data.js
@@ -1,52 +1,175 @@
 let data = {
-    caso_01: {
-        descricao: "Realizar soma entre dois números inteiros",
-        valor_01: 2,
-        valor_02: 2,
-        result: 4
+    texto: {
+        result: 'Resultado: ',
+        error: 'O valor é diferente do esperado: '
     },
-    caso_02: {
-        descricao: "Realizar soma com resultado negativo",
-        valor_01: 5,
-        valor_02: 3,
-        result: -2
+    operacoes: {
+        soma: '+',
+        subtracao: '-',
+        multiplicacao: '*',
+        divisao: '/',
+        potencia: '^'
     },
-    caso_03: {
-        descricao: "Realizar soma com números decimais",
-        valor_01: 0.5,
-        valor_02: 1.5,
-        result: 2
-    },
-    caso_04: {
-        descricao: "Realizar soma com número grandes",
-        valor_01: 99999,
-        valor_02: 11111,
-        result: 111110
-    },
-    caso_05: {
-        descricao: "Realizar soma por zero",
-        valor_01: 0,
-        valor_02: 1,
-        result: 11111
-    },
-    caso_06: {
-        descricao: "Subtracao de dois inteiros",
-        valor_01: 8,
-        valor_02: 3,
-        result: 5
-    },
-    caso_07: {
-        descricao: "Subtração de dois números negativos",
-        valor_01: -3,
-        valor_02: -8,
-        result: -11
-    },
-    caso_08: {
-        descricao: "Subtracao com resultado negativo",
-        valor_01: 3,
-        valor_02: 7,
-        result: -4
-    }
+    soma: [
+        {
+            descricao: "Realizar soma entre dois números inteiros positivos",
+            valor_01: 2,
+            valor_02: 2,
+            result: 4
+        },
+        {
+            descricao: "Realizar soma com resultado negativo",
+            valor_01: -5,
+            valor_02: 3,
+            result: -2
+        },
+        {
+            descricao: "Realizar soma com números decimais",
+            valor_01: 0.5,
+            valor_02: 1.5,
+            result: 2
+        },
+        {
+            descricao: "Realizar soma com número grandes",
+            valor_01: 99999,
+            valor_02: 11111,
+            result: 111110
+        },
+        {
+            descricao: "Realizar soma por zero",
+            valor_01: 0,
+            valor_02: 11111,
+            result: 11111
+        },
+    ],
+    subtracao: [
+        {
+            descricao: "Subtração de dois inteiros positivos",
+            valor_01: 8,
+            valor_02: 3,
+            result: 5
+        },
+        {
+            descricao: "Subtração de um número negativo e um positivo",
+            valor_01: -3,
+            valor_02: 8,
+            result: -11
+        },
+        {
+            descricao: "Subtração com resultado negativo",
+            valor_01: 3,
+            valor_02: 7,
+            result: -4
+        },
+        {
+            descricao: "Subtração de números decimais",
+            valor_01: 5.5,
+            valor_02: 2.5,
+            result: 3
+        },
+        {
+            descricao: "Subtração de um número com zero",
+            valor_01: 0,
+            valor_02: 9,
+            result: -9
+        }
+    ],
+    multiplicacao: [
+        {
+            descricao: "Multiplicacao de dois números naturais positivos",
+            valor_01: 6,
+            valor_02: 7,
+            result: 42
+        },
+        {
+            descricao: "Multiplicação de números negativos",
+            valor_01: -2,
+            valor_02: -5,
+            result: 10
+        },
+        {
+            descricao: "Multiplicação de números decimais",
+            valor_01: 3.5,
+            valor_02: 2.5,
+            result: '8,75'
+        },
+        {
+            descricao: "Multiplicação por zero",
+            valor_01: 0,
+            valor_02: 5,
+            result: 0
+        },
+        {
+            descricao: "Multiplicação por um",
+            valor_01: 1,
+            valor_02: 5,
+            result: 5
+        }
+    ],
+    divisao: [
+        {
+            descricao: "Divisão por zero",
+            valor_01: 9,
+            valor_02: 0,
+            result: 'Infinity'
+        },
+        {
+            descricao: "Divisor menor que dividendo",
+            valor_01: 3,
+            valor_02: 9,
+            result: '0,3333333333333333'
+        },
+        {
+            descricao: "Divisão de dois números decimais",
+            valor_01: 3.5,
+            valor_02: 7.2,
+            result: '0,4861111111111111'
+        },
+        {
+            descricao: "Divisão de dois números naturais positivos",
+            valor_01: 8,
+            valor_02: 2,
+            result: 4
+        },
+        {
+            descricao: "Divisão com dízima periódica",
+            valor_01: 13,
+            valor_02: 9,
+            result: '1,4444444444444444'
+        }
+    ],
+    potenciacao: [
+        {
+            descricao: "Potência de dois números inteiros positivos",
+            valor_01: 2,
+            valor_02: 3,
+            result: 8
+        },
+        {
+            descricao: "Potência de um número decimal e um inteiro",
+            valor_01: 3.7,
+            valor_02: 5,
+            result: "693,4395700000002"
+        },
+        {
+            descricao: "Potência de um número negativo",
+            valor_01: -3,
+            valor_02: 4,
+            result: 81
+        },
+        {
+            descricao: "Zero elevado a uma potência",
+            valor_01: 0,
+            valor_02: 5,
+            result: 0
+        },
+        {
+            descricao: "Número positivo elevado a zero",
+            valor_01: 5,
+            valor_02: 0,
+            result: 1
+        }
+    ]
     
 }
 
---
+++ main.js
@@ -0,0 +1,178 @@
+const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+
+const driver = new Builder().forBrowser('chrome').build();
+let path = 'https://www.calculadoraonline.com.br/basica';
+let data = require('./data')
+
+async function operacoes() {
+    try {   
+        // Abre a página da calculadora
+        await driver.get(path);
+        
+        // Encontra o elemento visor da calculadora
+        const divElement = await driver.findElement(By.id('LBSubResu'));
+
+        // Encontre o elemento <input> pelo ID "TIExp"
+        const inputElement = await driver.findElement(By.id('TIExp'));
+
+        // Execute um scroll para a exibição do elemento visor
+        await driver.executeScript("arguments[0].scrollIntoView();", divElement);
+        
+        // Linpa o visor
+        const tdElement = await driver.findElement(By.css('td[onclick*="limpa()"]'));
+        
+        // ----------------------------- SOMA -----------------------------------
+        
+        console.log('\n')
+        console.log('---------------------- SOMA -----------------------' + '\n')
+
+        
+        for (const item of data.soma) {
+          
+            await inputElement.sendKeys(item.valor_01);
+            await inputElement.sendKeys(data.operacoes.soma);
+            await inputElement.sendKeys(item.valor_02);
+            await inputElement.sendKeys(Key.RETURN);
+          
+            await driver.sleep(2000); // Aguarde 2 segundos
+          
+            let result_soma = await inputElement.getAttribute('value');
+          
+            if (result_soma == item.result.toString()) {
+              console.log(item.descricao);
+              console.log(data.texto.result + result_soma + '\n');
+            } else {
+              console.log(item.descricao);
+              console.log(data.texto.error + result_soma + '\n');
+            }
+          
+            await tdElement.click();
+        }
+
+        await tdElement.click()
+
+        // --------------------------- SUBTRAÇÃO -----------------------------
+        
+        console.log('\n')
+        console.log('---------------------- SUBTRAÇÃO -----------------------' + '\n')
+        
+        for (const item of data.subtracao) {
+          
+            await inputElement.sendKeys(item.valor_01);
+            await inputElement.sendKeys(data.operacoes.subtracao);
+            await inputElement.sendKeys(item.valor_02);
+            await inputElement.sendKeys(Key.RETURN);
+          
+            await driver.sleep(2000); // Aguarde 2 segundos
+          
+            let result_subtracao = await inputElement.getAttribute('value');
+          
+            if (result_subtracao == item.result.toString()) {
+              console.log(item.descricao);
+              console.log(data.texto.result + result_subtracao + '\n');
+            } else {
+              console.log(item.descricao);
+              console.log(data.texto.error + result_subtracao + '\n');
+            }
+          
+            await tdElement.click();
+        }
+
+        await tdElement.click();
+
+        // --------------------------------- MULTIPLICAÇÃO --------------------
+
+        console.log('\n')
+        console.log('---------------------- MULTIPLICAÇÃO -----------------------' + '\n')
+        
+        for (const item of data.multiplicacao) {
+          
+            await inputElement.sendKeys(item.valor_01);
+            await inputElement.sendKeys(data.operacoes.multiplicacao);
+            await inputElement.sendKeys(item.valor_02);
+            await inputElement.sendKeys(Key.RETURN);
+          
+            await driver.sleep(2000); // Aguarde 2 segundos
+          
+            let result_multiplicacao = await inputElement.getAttribute('value');
+          
+            if (result_multiplicacao == item.result.toString()) {
+              console.log(item.descricao);
+              console.log(data.texto.result + result_multiplicacao + '\n');
+            } else {
+              console.log(item.descricao);
+              console.log(data.texto.error + result_multiplicacao + '\n');
+            }
+          
+            await tdElement.click();
+        }
+
+        await tdElement.click();
+
+        // ------------------------------- DIVISÃO ---------------------------
+
+        console.log('\n')
+        console.log('---------------------- DIVISÃO -----------------------' + '\n')
+        
+        for (const item of data.divisao) {
+          
+            await inputElement.sendKeys(item.valor_01);
+            await inputElement.sendKeys(data.operacoes.divisao);
+            await inputElement.sendKeys(item.valor_02);
+            await inputElement.sendKeys(Key.RETURN);
+          
+            await driver.sleep(2000); // Aguarde 2 segundos
+          
+            let result_divisao = await inputElement.getAttribute('value');
+          
+            if (result_divisao == item.result.toString()) {
+              console.log(item.descricao);
+              console.log(data.texto.result + result_divisao + '\n');
+            } else {
+              console.log(item.descricao);
+              console.log(data.texto.error + result_divisao + '\n');
+            }
+          
+            await tdElement.click();
+        }
+
+        await tdElement.click();
+
+        // ----------------------- POTÊNCIAÇÃO ------------------------------
+
+        console.log('\n')
+        console.log('---------------------- POTÊNCIAÇÃO -----------------------' + '\n')
+
+        for (const item of data.potenciacao) {
+          
+            await inputElement.sendKeys(item.valor_01);
+            await inputElement.sendKeys(data.operacoes.potencia);
+            await inputElement.sendKeys(item.valor_02);
+            await inputElement.sendKeys(Key.RETURN);
+          
+            await driver.sleep(2000); // Aguarde 2 segundos
+          
+            let result_potenciacao = await inputElement.getAttribute('value');
+          
+            if (result_potenciacao == item.result.toString()) {
+              console.log(item.descricao);
+              console.log(data.texto.result + result_potenciacao + '\n');
+            } else {
+              console.log(item.descricao);
+              console.log(data.texto.error + result_potenciacao + '\n');
+            }
+          
+            await tdElement.click();
+        }
+
+        await tdElement.click();
+
+    } catch (error) {
+        console.log('Error:', error)
+    } finally {
+        await driver.quit();
+    }
+}
+
+
+operacoes();
